e7448b3949f43efda9f04b57125ff44349dc68c3
Your Name <you@example.com>
2022-01-30 20:30:11 -0300

    Terminado

---
+++ src/herenciaconstructores/Moto.java
@@ -0,0 +1,17 @@
+package herenciaconstructores;
+
+/**
+ *
+ * @author Charly Cimino Aprendé más Java en mi canal:
+ * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
+ * GitHub: https://github.com/CharlyCimino
+ */
+public class Moto extends Vehiculo {
+
+    private int anchoDeManubrio;
+
+    // TAREA: Implementá el constructor de 'Moto' siguiendo las pautas de 'Auto'.
+    public void hacerWheelie() {
+        System.out.println("Haciendo la wheelie...");
+    }
+}
